e10380cd10c77ffa0e03bb2e5f092e8f42cfd187
davake01 <david.akesson@sr.se>
2022-09-21 22:50:46 +0200

    really working in windows

--- slowcat.py
+++ slowcat.py
@@ -1,19 +1,28 @@
 from ast import Try
 import os
-
+import time
 
 path = './'
 
 #files = os.listdir(path)
-
-for root, directories, files in os.walk(path, topdown=False):
-	for name in files:
-		if not name.startswith('.'):
-			try:
-				with open(str(os.path.join(root, name)), 'r') as f: 
-					print(f.read())
-			except:
-				pass
+try:
+	for root, directories, files in os.walk(path, topdown=False):
+		for name in files:
+			if not name.startswith('.'):
+				try:
+					with open(str(os.path.join(root, name)), 'r') as f: 
+						#print(f.read())
+						while True:
+							next_line = f.readline()
+							if not next_line:
+								break;
+							print(next_line.strip())
+							time.sleep(0.1)
+						f.close()
+				except:
+					pass
+except KeyboardInterrupt:
+	pass
 		#print(os.path.join(root, name))
 	#for name in directories:
 	#	print(os.path.join(root, name))
