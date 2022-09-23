a9fe96f7d5181b205243609c4c9dcd62897a6edf
akeda <akeda2@gmail.com>
2022-09-23 14:50:02 +0200

    char by char

--- slowcat.py
+++ slowcat.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 from ast import Try
 import os
 import time
@@ -12,29 +13,30 @@ if len(sys.argv) > 1:
 
 SLEEPTIME = int(SOURCE) / 1000
 #files = os.listdir(path)
-try:
-	for root, directories, files in os.walk(path, topdown=False):
-		for name in files:
-			if not name.startswith('.'):
-				try:
-					with open(str(os.path.join(root, name)), 'r') as f: 
-						#print(f.read())
-						while True:
-							next_line = f.readline()
-							if not next_line:
-								break;
-							print(next_line.strip("\n"))
-							time.sleep(SLEEPTIME)
-						f.close()
-				except:
-					pass
-except KeyboardInterrupt:
-	#raise SystemExit
-	sys.exit()
-		#print(os.path.join(root, name))
-	#for name in directories:
-	#	print(os.path.join(root, name))
+while True:
+    try:
+        for root, directories, files in os.walk(path, topdown=False):
+            for name in files:
+                if not name.startswith('.'):
+                    try:
+                        with open(str(os.path.join(root, name)), 'r') as f: 
+                            #print(f.read())
+                            while True:
+                                next_line = f.readline()
+                                if not next_line:
+                                    break;
+                                #print(next_line.strip("\n"))
+                                for char in next_line:
+                                    print(char, end='', flush=True)
+                                    time.sleep(0.005)
+                                time.sleep(SLEEPTIME)
+                            f.close()
+                    except:
+                        pass
+    except KeyboardInterrupt:
+        #raise SystemExit
+        sys.exit()
+            #print(os.path.join(root, name))
+        #for name in directories:
+        #	print(os.path.join(root, name))
 
-
-#for f in files:
-#	print(f)
