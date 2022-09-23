7a5e36bc37dc384f2929dd7ecad7779d161d5e84
akeda <akeda2@gmail.com>
2022-09-23 16:19:21 +0200

    optional filename

--- slowcat.py
+++ slowcat.py
@@ -5,10 +5,11 @@ import time
 import sys
 import argparse
 
-path = './'
+
 
 parser = argparse.ArgumentParser(description="slowcat - slow concatenation",
                                   formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser.add_argument("filename", default=None, nargs='?', help="Filename, if omitted - walk tree")
 parser.add_argument("-c", "--char", action="store_true", help="Character mode")
 parser.add_argument("-l", "--line", default=True, action="store_true", help="Line mode")
 parser.add_argument("-s", "--sleep", default=51, type=int, help="Time to sleep between lines")
@@ -18,6 +19,11 @@ config = vars(args)
 print("slowcat - arguments:")
 print(config)
 
+if args.filename:
+    path = args.filename
+else:
+    path = './'
+#print(path)
 # mSec to sleep between printed lines
 #SOURCE = 50
 #if len(sys.argv) > 1:
@@ -27,10 +33,35 @@ time.sleep(2)
 #SLEEPTIME = int(SOURCE) / 1000
 SLEEPTIME = args.sleep / 1000
 #files = os.listdir(path)
+if args.filename:
+    try:
+        with open((args.filename), 'r') as f: 
+            #print(f.read())
+            while True:
+                next_line = f.readline()
+                if not next_line:
+                    break;
+                if args.line:
+                    print(next_line.strip("\n"))
+                elif args.char:
+                    for char in next_line:
+                        print(char, end='', flush=True)
+                        time.sleep(args.pace)
+                        time.sleep(0.003)
+                if args.line and args.sleep == 51:
+                    time.sleep(SLEEPTIME*2)
+                else:
+                    time.sleep(SLEEPTIME)
+            f.close()
+    except:
+        pass
+    exit()
+
 while True:
     try:
         for root, directories, files in os.walk(path, topdown=False):
             for name in files:
+                print(name)
                 if not name.startswith('.'):
                     try:
                         with open(str(os.path.join(root, name)), 'r') as f: 
