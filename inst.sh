b777996f6dcfbb27a646e62d92f8d51fd7ae61f0
akeda <akeda2@gmail.com>
2022-12-15 17:23:01 +0100

    install by copy

--- inst.sh
+++ inst.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-[ -f slowcat.py ] && sudo ln -s $(realpath slowcat.py) /usr/local/bin/slowcat && echo "Done"
+[ -f slowcat.py ] && sudo cp --remove-destination slowcat.py /usr/local/bin/slowcat && echo "Done" || echo "FAIL!"
